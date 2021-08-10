94384b93eed577e520053f5e04005f248b808ae7
Mehmet Seven <mehmetseven0@gmail.com>
2021-08-10 15:41:18 +0300

    drawer navigator

--- NavigationApp/src/navigation/DrawerNavigator.js
+++ NavigationApp/src/navigation/DrawerNavigator.js
@@ -1,15 +1,38 @@
 import React from 'react';
-
+import {View, Text} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import {SettingsStackNavigator, SearchStackNavigator} from './StackNavigator';
 import TabNavigator from './TabNavigator';
 
+function DrawerContent() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#f8f8f8',
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        paddingVertical: 40,
+      }}>
+      <Text>Copyright 2020 </Text>
+    </View>
+  );
+}
+
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator screenOptions={{headerShown: false}}>
+    <Drawer.Navigator
+      drawerContent={DrawerContent}
+      screenOptions={{
+        headerShown: false,
+        drawerType: 'slide',
+        drawerStyle: {
+          width: 200,
+        },
+      }}>
       <Drawer.Screen
         name="HomeDrawer"
         component={TabNavigator}
